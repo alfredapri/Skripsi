835d3413dd6fa12458d03ea8386c3f554b6817dc
alfredapri <aaliaunardi@gmail.com>
2022-10-14 14:25:42 +0700

    Create basic program repository

---
+++ source/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Alfred/Studies/College/Skripsi/repo/source
+# Build directory: D:/Alfred/Studies/College/Skripsi/repo/source/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
