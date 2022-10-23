e8a22bcde2b61d5660094580d537fd8d6e916f32
alfredapri <aaliaunardi@gmail.com>
2022-10-23 21:30:43 +0700

    Ecluded build files
    Updated .gitignore to exclude build files

--- source/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Alfred/Studies/College/Skripsi/repo/source
-# Build directory: D:/Alfred/Studies/College/Skripsi/repo/source/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
